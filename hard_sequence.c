e331918bf8b3e11f9e2395cfe446963b784bac83
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2019-11-02 02:35:01 +0530

    Create hard_sequence.c

---
+++ hard_sequence.c
@@ -0,0 +1,36 @@
+/**
+Problem link: https://www.codechef.com/NOV19B/problems/HRDSEQ
+**/
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int arr[128], n, k, i, j;
+        int count=1;
+        scanf("%d ",&n);
+        arr[0]=0;
+        arr[1]=0;
+        for(i=2;i<n;i++){
+            k=i-1;
+            for(j=i-2;j>=0;j--){
+                if(arr[j]==arr[k]){
+                    arr[i]=k-j;
+                    break;
+                }
+                else{
+                    arr[i]=0;
+                }
+            }
+        }
+        for(i=0;i<n-1;i++){
+            if(arr[i]==arr[n-1]){
+                count++;
+            }
+        }
+        printf("%d ",count);
+    }
+	return 0;
+}
+
